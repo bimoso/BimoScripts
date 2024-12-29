199dcad538c1ca159fa9717d973152c4abf5a9b6
Bimo <elnonino23@gmail.com>
2024-12-29 11:35:25 -0600

    Actualize el de BF

--- BloxFruits.lua
+++ BloxFruits.lua
@@ -1,12 +1,18 @@
-_G["HoHo Fruits Collector"] = {
-    ["Select Team"] = "Marine", -- "Pirate" or "Marine"
-    ["Auto Store"] = true, -- fck, why i need to make this it should be always true >.<
-    ["Buy Fruit From Dealer"] = true, -- fck, why i need to make this it should be always true >.<
-    ["Ignore Fruits Spawned That You Already Have"] = true, -- fck, why i need to make this it should be always true >.<
-    ["Bypass Teleport"] = true, -- faster farming :3
-    ["Webhook"] = {
-        ["Enable"] = false, --useful
-        ["Webhook Link"] = "",
-    },
+_G.HoHoCustomConfig = {
+    SelectTeam = "Marine",
+    BypassTeleport = false,
+    PortalTeleport = true,
+    LeavePortalUlti = true,
+    FPSBoost = false,
+    WhiteScreen = false,
+    AutoAlly = true,
+    NoClip = true,
+    NoStun = true,
+    AutoCollectFruit = true,
+    AutoRandomFruit = true,
+    CheckFruitStoreable = true,
+    Webhook = "https://discord.com/api/webhooks/1321976551134199928/RMHcjSEoYjN56f96x-P9Gqtuw5QDd1MzyIEzEUoPAFBQ66_6B2qGWjzBRKoB1A2gPcab",
+    SendWebhookNewItem = true,
+    FarmWithHop = true,
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
