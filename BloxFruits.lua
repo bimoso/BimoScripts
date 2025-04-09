dde1ceb64eb2fbf47663ad06f4f516130358438a
Bimo <129549698+bimoso@users.noreply.github.com>
2025-04-09 01:59:43 -0600

    Update BloxFruits.lua

--- BloxFruits.lua
+++ BloxFruits.lua
@@ -1,9 +1,9 @@
 local Settings = {
-    JoinTeam = "Pirates"; -- Pirates/Marines
-    Translator = true; -- true/false
-  }
-  
-loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))(Settings)
 local Workspace = game:GetService("Workspace")
 local UserInputService = game:GetService("UserInputService")
 local Players = game:GetService("Players")
