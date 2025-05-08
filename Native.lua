1123ec3639f51e85e505dc2d4c5c07705070dad0
Bimo <129549698+bimoso@users.noreply.github.com>
2025-05-08 17:24:25 -0600

    Update Native.lua

--- Native.lua
+++ Native.lua
@@ -1,3 +1,20 @@
+local placeId = game.PlaceId
+
+local args = {
+	{
+		trainId = "default",
+		maxMembers = 1,
+		gameMode = "Normal"
+	}
+}
+
 script_key="qXYdOmXCQPyzBzgTYjIqsVrXuvhvwUAm";
 
 (loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
+
+if placeId == 116495829188952 then
+    while wait(0.5) do
+        game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Network"):WaitForChild("RemoteEvent"):WaitForChild("CreateParty"):FireServer(unpack(args))
+    end
+    
+do
