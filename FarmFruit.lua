b15427635bbd2b2a8bc5deb8e2fe8b592915eb4a
bimoso <elnonino23@gmail.com>
2025-10-06 22:12:06 -0600

    \

---
+++ FarmFruit.lua
@@ -0,0 +1,19 @@
+_G.HoHoCustomConfig = {
+    SelectTeam = "Marine",
+    BypassTeleport = false,
+    PortalTeleport = true,
+    LeavePortalUlti = true,
+    FPSBoost = true,
+    WhiteScreen = true,
+    AutoAlly = true,
+    NoClip = true,
+    NoStun = true,
+    AutoCollectFruit = true,
+    AutoRandomFruit = true,
+    CheckFruitStoreable = true,
+    Webhook = "https://discord.com/api/webhooks/1324868699584335892/OZSOrH-juPYMzAcZ4F78E3PXtHnc8q3SteoGHvPyb_AaCQuaXrO7L3IfIs8XIQGjbWDX",
+    SendWebhook = true,
+    SendWebhookNewItem = true,
+    FarmWithHop = true,
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
